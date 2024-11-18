0be254662983a8f988e927caaf242f1972b703d8
miroslav.komar <miroslav.komar@isdd.sk>
2024-11-18 16:49:51 +0100

    removed location id index

--- uu_rehearsalmanager_maing01-server/app/dao/location-mongo.js
+++ uu_rehearsalmanager_maing01-server/app/dao/location-mongo.js
@@ -4,7 +4,6 @@ const { UuObjectDao } = require("uu_appg01_server").ObjectStore;
 
 class LocationMongo extends UuObjectDao {
     async createSchema() {
-        await super.createIndex({ awid: 1, id: 1 }, { unique: true });
         await super.createIndex( { awid: 1, actId: 1});
     }
 
