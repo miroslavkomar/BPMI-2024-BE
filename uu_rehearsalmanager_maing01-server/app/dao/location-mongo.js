d86cb08d5bab38ad8f3839bcb6f0cf0b767656c7
Michaela Krčilová <michaela.krcilova@unicornuniversity.net>
2024-12-03 10:55:48 +0100

    fixed minor index error in location-mongo.js

--- uu_rehearsalmanager_maing01-server/app/dao/location-mongo.js
+++ uu_rehearsalmanager_maing01-server/app/dao/location-mongo.js
@@ -4,7 +4,7 @@ const { UuObjectDao } = require("uu_appg01_server").ObjectStore;
 
 class LocationMongo extends UuObjectDao {
     async createSchema() {
-        await super.createIndex( { awid: 1, actId: 1});
+        await super.createIndex( { awid: 1, active: 1 });
     }
 
     async list(awid, active, pageInfo) {
